b6d261caaa2edb68c6e3e334ba8a28b99a6110b3
Jefferson <jefferson-67@live.com>
2022-08-16 17:02:26 -0300

    infra layer +migration+update on db = done

---
+++ Infrastructure/DBConfiguration/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DBConfiguration
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Food> Dishes { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) 
+            : base(options) { }
+        public ApplicationContext() { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //TODO
+        }
+    }
+}
---
+++ Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,71 @@
+﻿using Domain.Interfaces.Repositories;
+using Infrastructure.DBConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbSet<TEntity> DbSet;
+        protected readonly ApplicationContext Db;
+
+        public Repository(ApplicationContext context)
+        {
+            Db = context;
+            DbSet = Db.Set<TEntity>();
+        }
+        public void Add(TEntity obj)
+        {
+            DbSet.Add(obj);
+            SaveChanges();
+        }
+
+        public bool Any(Func<TEntity, bool> exp)
+        {
+            return DbSet.Any(exp);
+        }
+
+        public void Dispose()
+        {
+            Db.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return DbSet.AsNoTracking(); ;
+        }
+
+        public IQueryable<TEntity> GetAllBy(Func<TEntity, bool> exp)
+        {
+            return DbSet.Where(exp).AsQueryable();
+        }
+
+        public TEntity GetBy(Func<TEntity, bool> exp)
+        {
+            return DbSet.FirstOrDefault(exp);
+        }
+
+        public TEntity GetById(Guid id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public void Remove(Guid id)
+        {
+            DbSet.Remove(DbSet.Find(id));
+            SaveChanges();
+        }
+
+        public int SaveChanges()
+        {
+            return Db.SaveChanges();
+        }
+
+        public void Update(TEntity obj)
+        {
+            DbSet.Update(obj);
+            SaveChanges();
+        }
+    }
+}
